468fcc5486978bdeaa7d2708866c9974bdb4947f
João Guilherme <joaorato58@gmail.com>
2025-10-01 22:00:49 -0300

    refactor: Separate Music and SFX volume using audio buses

--- battle/scenes/options.gd
+++ battle/scenes/options.gd
@@ -1,21 +1,33 @@
 extends Control
 
-@onready var master_slider = $MarginContainer/OptionsContainer/VolumeVContainer/SoundControl
+@onready var music_slider = $MarginContainer/OptionsContainer/VolumeVContainer/MusicControl
+@onready var sfx_slider = $MarginContainer/OptionsContainer/VolumeVContainer/SFXControl
 
-var master_bus = AudioServer.get_bus_index("Master")
+var music_bus = AudioServer.get_bus_index("Music")
+var sfx_bus = AudioServer.get_bus_index("SFX")
 
 func _ready() -> void:
-	var current_volume = AudioServer.get_bus_volume_db(master_bus)
-	master_slider.value = current_volume
+	var current_music_volume = AudioServer.get_bus_volume_db(music_bus)
+	music_slider.value = current_music_volume
+	
+	var current_sfx_volume = AudioServer.get_bus_volume_db(sfx_bus)
+	sfx_slider.value = current_sfx_volume
 
 func _on_sound_control_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(master_bus, value)
+	AudioServer.set_bus_volume_db(music_bus, value)
 	
 	if value == -30:
-		AudioServer.set_bus_mute(master_bus, true)
+		AudioServer.set_bus_mute(music_bus, true)
 	else:
-		AudioServer.set_bus_mute(master_bus, false)
+		AudioServer.set_bus_mute(music_bus, false)
 
+func _on_sfx_control_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(sfx_bus, value)
+	
+	if value == -30:
+		AudioServer.set_bus_mute(sfx_bus, true)
+	else:
+		AudioServer.set_bus_mute(sfx_bus, false)
 
 func _on_return_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
