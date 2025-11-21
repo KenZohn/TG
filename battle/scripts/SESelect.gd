34ce05e3859629b631535c15ef2605f0012569fb
João Guilherme <joaorato58@gmail.com>
2025-11-20 23:15:46 -0300

    refactor: integra áudio dos botões ao bus SFX

--- battle/scripts/SESelect.gd
+++ battle/scripts/SESelect.gd
@@ -5,6 +5,7 @@ var click_sound : AudioStream = preload("res://assets/se/maou_se_system48.mp3")
 func play():
 	var player = AudioStreamPlayer.new()
 	player.stream = click_sound
+	player.bus= "SFX"
 	add_child(player)
 	player.play()
 	player.connect("finished", Callable(player, "queue_free"))
